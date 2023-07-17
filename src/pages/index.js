e63a73e26bbfa0f3472796c92071e781a69a2500
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-07-17 14:11:01 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,74 +7,56 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 import { useData } from '../theme/Root';
 
+async function authorize(login, password) {
+  const response = await fetch('https://goiteens-platform.vercel.app/api/authorization', {
+    method: 'POST',
+    body: JSON.stringify({ login, password }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return response.json();
+}
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  const {data, setData} = useData()
+  const { siteConfig } = useDocusaurusContext();
+  const { data, setData } = useData();
 
   const auth = async (e) => {
     e.preventDefault();
-      const response = await fetch('https://goiteens-platform.vercel.app/api/authorization', {
-        headers: {
-"Accept": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*",
-          'Content-Type': 'application/json',
-        },
-        method: 'POST',
-        body: JSON.stringify({
-          login: e.target[0].value,
-          password: e.target[1].value,
-        }),
+    const login = e.target[0].value;
+    const password = e.target[1].value;
+    try {
+      const result = await authorize(login, password);
+      console.log(result);
+      setData({
+        status: result[0].status,
+        login: e.target[0].value,
+        idRoom: result[1].idRoom,
       });
-      const movies = await response.json();
-      console.log(movies);
-    // fetch('https://goiteens-platform.vercel.app/api/authorization', {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     login: e.target[0].value,
-    //     password: e.target[1].value,
-    //   }),
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // })
-    //   .then((response) => response.json())
-    //   .then((data2) =>{
-    //   console.log(data2);
-    //     setData({
-    //       status: data2[0].status,
-    //       login: e.target[0].value,
-    //       idRoom: data2[1].idRoom,
-    //     })}
-    //   )
-    //   .catch((error) => {
-    //     // Handle the error here
-    //     console.error(error);
-    //   });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-
-return (
-  <header className={clsx('hero hero--primary', styles.heroBanner)}>
-    <div className="container">
-      <p>{data.status}</p>
-    <form onSubmit={auth}>
-    <input type="text" placeholder="login" required/>
-    <input type="password" placeholder="password" required/>
-    <button>Submit</button>
-</form>
-</div>
-</header>
-)
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <p>{data.status}</p>
+        <form onSubmit={auth}>
+          <input type="text" placeholder="login" required />
+          <input type="password" placeholder="password" required />
+          <button>Submit</button>
+        </form>
+      </div>
+    </header>
+  );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
